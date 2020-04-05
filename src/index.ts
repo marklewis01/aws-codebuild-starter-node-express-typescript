adb9ab95c9e000098da23d0dbcd071face755aad
Mark Lewis <22421135+marklewis01@users.noreply.github.com>
2020-04-05 10:47:42 +1000

    removing logger.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { logger } from "./shared";
+// import { logger } from "./shared";
 import server from "./server";
 
 // Server
 const port = process.env.PORT || 3001;
 
 server.listen(port, () => {
-  logger.info(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
