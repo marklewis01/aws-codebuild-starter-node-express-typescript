821b18a5f82e6c026a80aa9ae887941db435ad51
Mark Lewis <22421135+marklewis01@users.noreply.github.com>
2020-04-05 19:03:20 +1000

    adding default root html

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import fs from "fs";
 
 import BaseRouter from "./routes";
+
+const html = fs.readFileSync("./src/public/index.html");
 
 /**
  * Init Express
@@ -17,6 +20,15 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", BaseRouter); // API Endpoint
+
+/**
+ * Default root response
+ */
+app.use("/", (req, res) => {
+  res.writeHead(200);
+  res.write(html);
+  res.end();
+});
 
 /**
  * Export Express instance
